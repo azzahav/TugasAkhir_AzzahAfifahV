78823331de2c42744e47abfcb13c3b5d87706605
azzahav <50991025+azzahav@users.noreply.github.com>
2019-05-24 20:09:51 +0700

    Add files via upload

---
+++ TugasAkhirBeneran_Azzah/src/tugasakhirbeneran_azzah/Exit.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugasakhirbeneran_azzah;
+
+/**
+ *
+ * @author AZZAH AFIFAH
+ */
+public class Exit {
+    public void Keluar (){
+        System.exit(0);
+    }
+}
